66ff6b2f63175e9c4755852d20284512ee7a9a24
antimatterbeer <antimatter_beer@outlook.com>
2023-10-10 17:08:39 +0800

    Update demo logger

--- demo/logger.hpp
+++ demo/logger.hpp
@@ -12,23 +12,28 @@ public:
     LOG_INFO("[Logger]Init");
     return true;
   }
+
   void OnSession(const base::Session &session) override {
     LOG_INFO("[Logger]OnSession");
   }
+
   void OnBar(const base::Bar &bar) override {
     LOG_INFO("[Logger]OnBar. open: {} high: {} low: {} close: {} volume: {} "
              "turnover: {}",
              bar.open_, bar.high_, bar.low_, bar.close_, bar.volume_,
              bar.turnover_);
   }
+
   void OnStatic(const base::Static &st) override {
     LOG_INFO("[Logger]OnStatic. instrument_id: {} prev_close: {}",
              st.instrument_id_, st.prev_close_);
   }
+
   void OnDepth(const base::Depth &depth) override {
     LOG_INFO("[Logger]OnDepth. instrument_id: {} update_time: {}",
              depth.instrument_id_, depth.update_time_.ToString());
   }
+  
   void OnEndOfDay(const base::Date &Date) override {
     LOG_INFO("[Logger]OnEndOfDay. date: {}", Date.ToString());
   }
